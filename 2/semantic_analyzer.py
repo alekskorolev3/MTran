5c719881cd1ed2ba89708eaa8c1c8353c534ddb0
alekskorolev3 <alekskorolev3@gmail.com>
2023-04-04 23:22:39 +0300

    Added indexing_op and array_init

--- 2/semantic_analyzer.py
+++ 2/semantic_analyzer.py
@@ -42,15 +42,37 @@ class SemanticAnalyzer:
                 rt = self.gettype(right_op)
 
             if lt != rt:
-                raise SyntaxError("Mismatch types " + str(lt) + " and " + str(rt))
+                raise SyntaxError("Mismatch types in " + str(tree_type) + ": " + str(lt) + " and " + str(rt))
             else:
                 return lt
 
         if tree_type == "arg":
             if _type is not None:
                 if type(children[0]).__name__ != _type:
-                    raise SyntaxError("Mismatched types" + str(children[0]))
+                    raise SyntaxError("Type " + str(_type) + " cannot initialize " + str(children[0]))
                 return
+
+        if tree_type == "array_init":
+            self.analyze(children[0], _type)
+            return
+
+        if tree_type == "init_block":
+            for child in children:
+                if child != ",":
+                    if self.gettype(child) != _type:
+                        raise SyntaxError("Wrong initialization of array")
+            return
+
+        if tree_type == "indexing_op":
+
+            if children[0] in self.variables.keys():
+                if self.variables.get(children[0]).get("type") != _type:
+                    raise SyntaxError("Mismatch types on initialization")
+
+            if self.gettype(children[1]) != _type:
+                raise SyntaxError("Wrong argument type in indexing operation")
+
+            return
 
         if tree_type == "init":
             self.variables[children[1]] = {"type": children[0], "value": children[1]}
@@ -62,7 +84,6 @@ class SemanticAnalyzer:
         if tree_type == "assign":
             self.analyze(children[-1], self.variables.get(children[0]).get("type"))
             return
-
 
         for child in children:
             if child != "=":
